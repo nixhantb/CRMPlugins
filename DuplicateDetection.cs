20f14d0d84c0961d0eed2fd2d4f2b869be67667c
nixhantb <nishantbanjade07@gmail.com>
2025-06-18 21:39:21 +0100

    update duplicate detection code

---
+++ DuplicateDetection.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Query;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRM
+{
+    public class DuplicateDetection : IPlugin
+    {
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            // Obtain the tracing service
+            ITracingService tracingService =
+            (ITracingService)serviceProvider.GetService(typeof(ITracingService));
+
+            // Obtain the execution context from the service provider.  
+            IPluginExecutionContext context = (IPluginExecutionContext)
+                serviceProvider.GetService(typeof(IPluginExecutionContext));
+
+            // The InputParameters collection contains all the data passed in the message request.  
+            if (context.InputParameters.Contains("Target") &&
+                context.InputParameters["Target"] is Entity)
+            {
+                // Obtain the target entity from the input parameters.  
+                Entity contact = (Entity)context.InputParameters["Target"];
+
+                // Obtain the IOrganizationService instance which you will need for  
+                // web service calls.  
+                IOrganizationServiceFactory serviceFactory =
+                    (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
+                IOrganizationService service = serviceFactory.CreateOrganizationService(context.UserId);
+
+                try
+                {
+
+
+                    string email = string.Empty;
+
+                    if (contact.Attributes.Contains("emailaddress1"))
+                    {
+                        email = contact.Attributes["emailaddress1"].ToString();
+
+                        QueryExpression queryExp = new QueryExpression("contact");
+
+                        queryExp.ColumnSet = new ColumnSet(new string[] { "emailaddress1" });
+                       
+                        queryExp.Criteria.AddCondition("emailaddress1", ConditionOperator.Equal, email);
+
+                        EntityCollection results = service.RetrieveMultiple(queryExp);
+
+                        if (results.Entities.Count > 0)
+                        {
+                            throw new InvalidPluginExecutionException("A contact with this email address already exists.");
+                        }
+
+                    }
+                }
+                catch (FaultException<OrganizationServiceFault> ex)
+                {
+                    throw new InvalidPluginExecutionException("An error occurred in FollowUpPlugin.", ex);
+                }
+
+                catch (Exception ex)
+                {
+                    tracingService.Trace("FollowUpPlugin: {0}", ex.ToString());
+                    throw;
+                }
+            }
+        }
+    }
+}
